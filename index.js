d7eb6372252b4fec7ecfd6b1dfc1e7776788b19e
Nolan Welch <bugygamer.mc@gmail.com>
2023-01-15 18:20:54 -0700

    commit tree 12b65f91f54c4fd4d2de0937baf35900e3290de6

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+startnumber == 10
+
+function countDown () {
+    for (let i = 0; i <=10; i++)
+        console.log(i)
+}
+
+
+
+countDown ();
+
+const names = [ "Guadalupe", "Ollie", "Aki"];
+
+function writeCards ([names],x) {
+    for (let i = 0; i <names.length; i++) {
+    console.log (`"Thank you, ${names[i]},for the wonderful ${x} gift!"`);
+    }
+    names;
+}
+writeCards ([names], `Birthday`);
+const  messages = [writeCards ([names],`Birthday`)];
+messages[2];
